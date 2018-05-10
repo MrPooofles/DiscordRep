ebce2d4001bb172a58217bbfd50bab5cf14898f4
MrPooofles <38237033+MrPooofles@users.noreply.github.com>
2018-05-10 11:30:05 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,27 +6,34 @@ client.on('ready', () => {
     });
 
 client.on('message', message => {
-    if (message.content === '.infopoof') {
+    if (message.content === '.helpme') {
+    	message.reply('commands that i know: .server(will show you the ip and port of every server!) .report(will help you to report someone that did not follow the rules!)');
+  	}
+    
+    });
+
+client.on('message', message => {
+    if (message.content === '.info') {
     	message.reply('I have no complex commands! But i can give you information. Do .helpme for help');
   	}
     
     });
 
 client.on('message', message => {
-    if (message.content === 'owner') {
+    if (message.content === '.owner') {
     	message.reply('Mr.Poof is my Owner!');
   	}
     
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '.ping') {
     	message.channel.send('PONG!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
+    if (message.content === '.bing') {
     	message.reply('BONG!');
   	}
 });
